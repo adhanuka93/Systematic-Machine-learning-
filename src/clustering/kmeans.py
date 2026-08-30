3779d4a9ca9593076c11b91c57673d184cf816f0
Ankit Dhanuka <adhanuka@illinois.edu>
2026-08-30 14:26:47 -0500

    Add files via upload

---
+++ src/clustering/kmeans.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+class k_means():
+    def __init__(self, K=3, method='kmeans++',max_iter = 1000,tol = 1e-4):
+        self.K = K 
+        self.max_iter = max_iter
+        self.tol = tol
+        self.centroids = None
+        self.method = method
+        self.labels = None
+
+    def _kmeans_plus_plus(self, X):
+        N, D = X.shape
+        first_index = np.random.choice(N)
+        self.centroids = np.zeros((self.K, D))
+        self.centroids[0] = X[first_index].copy()
+
+        min_distance = np.full(N, np.inf)
+        for i in range(1, self.K):
+            current_centroid = self.centroids[i-1]
+            distance = np.sum((X-current_centroid)**2, axis =1)
+            min_distance = np.minimum(min_distance, distance)
+            tot_distance = np.sum(min_distance)
+            if tot_distance==0:
+                prob = np.full(N, 1.0/N)
+            else:
+                prob = min_distance/tot_distance
+
+            chosen_idx = np.random.choice(N, p = prob)
+            self.centroids[i] = X[chosen_idx]
+
+        return self
+
+
+    def fit(self, X):
+        N, D = X.shape
+
+        if self.method=='kmeans++':
+            self._kmeans_plus_plus(X)
+        else:
+
+            idx = np.random.choice(N, self.K, replace=False)
+            self.centroids = X[idx].copy()
+
+
+        for iteration in range(self.max_iter):
+            prev_centroids = self.centroids.copy()
+            # distance = np.linalg.norm(X[:, None, :] - self.centroids[None, :, :], axis = 2, keepdims=False)
+
+            X_sqr = np.sum(X**2, axis =1)
+            Centroid_sqr = np.sum(self.centroids**2, axis=1)
+            distance = np.maximum(X_sqr[:, None] + Centroid_sqr[None, :] - 2*(X@self.centroids.T), 0.0)
+
+            self.labels=np.argmin(distance, axis=1)
+
+            one_hot = np.zeros((N, self.K))
+
+            one_hot[np.arange(N), self.labels] = 1.0
+            counts = np.sum(one_hot, axis =0)
+
+            non_empty_clusters = (counts>0)
+            self.centroids[non_empty_clusters] = (one_hot.T[non_empty_clusters]@X)/counts[non_empty_clusters, None]
+
+            if np.any(counts == 0):
+                empty_indices = np.where(~non_empty_clusters)[0]
+                num_empty = len(empty_indices)
+
+                # Find points farthest from their assigned centroid
+                min_distance_to_centroid = np.min(distance, axis=1)
+                farthest_indices = np.argsort(min_distance_to_centroid)[
+                    -num_empty:
+                ]
+
+                self.centroids[empty_indices] = X[farthest_indices]
+
+
+            if iteration>0:
+                if np.sum((self.centroids-prev_centroids)**2)<self.tol:
+                    break
+
+        return self
+
+
+    def predict(self, X):
+        X_sqr = np.sum(X**2, axis=1, keepdims=True)
+        Centroid_sqr = np.sum(self.centroids**2, axis=1, keepdims=True).T
+        distance = np.maximum(
+            0.0, X_sqr + Centroid_sqr - 2 * (X @ self.centroids.T)
+        )
+        return np.argmin(distance, axis=1)
+
